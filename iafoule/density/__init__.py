83ab40d5fb95cef579230fcbbaf5aaed496d4397
titigmr <thierry.gmr@outlook.fr>
2022-01-25 10:15:57 +0000

    fix import

--- iafoule/density/__init__.py
+++ iafoule/density/__init__.py
@@ -1,5 +1,5 @@
-from ._density_maps import (get_img_pathes, get_gt_dots
+from ._density_maps import (get_img_pathes, get_gt_dots,
                             compute_distances, generate_gaussian_kernels,
                             compute_sigma, find_closest_key,
                             gaussian_filter_density, save_computed_density,
-                            DensityMap, show_image_with_density, load_sparse)
+                            DensityMap, show_image_with_density)
