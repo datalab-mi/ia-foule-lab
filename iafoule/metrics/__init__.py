84dc228e4cd5cf0165f01ae2f0fc2b26e4536139
fabricejourdan <jourdan.fabrice@yahoo.fr>
2022-03-09 15:31:16 +0100

    maj __init__

--- iafoule/metrics/__init__.py
+++ iafoule/metrics/__init__.py
@@ -1,1 +1,2 @@
-from ._metrics import (fonction_test)
+from ._metrics import (get_benchmark_metrics, get_metrics, get_grid_metrics, get_metrics_with_points,
+                       get_grid_metrics_with_points)
