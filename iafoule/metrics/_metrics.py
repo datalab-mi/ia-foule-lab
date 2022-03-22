aad7ca90a5decac70e44bc6cf3cb8099c9110bd4
fabricejourdan <jourdan.fabrice@yahoo.fr>
2022-03-22 10:27:30 +0000

    corrections, menage, commentaires

--- iafoule/metrics/_metrics.py
+++ iafoule/metrics/_metrics.py
@@ -3,8 +3,20 @@ import math
 import numpy as np
 
 
-def get_metrics(prediction, ground_truth, metric_grids=None, debug=False):
+def get_metrics(prediction, ground_truth, metric_grids=None):
+    """
+    get metrics between prediction and ground truth for following types (int, float, np array)
+    metrics are absolute_error (AE), absolute_percentage_error (APE), squared_error (SE)
+    and for metrics grid (only for np array) gridNxN_absolute_percentage_error (GAPE) and  gridNxN_cell_absolute_error (GCAE) 
+    args:
+        - prediction: prediction value (int, float, np array)
+        - ground_truth: ground truth value (int, float, np array)
+        - metric_grids: List of tuple [(2,2),(4,4)], default None.
+    """
+    
     metrics = dict()
+    if metric_grids is None:
+        metric_grids = []
     if (isinstance(prediction, int) and isinstance(prediction, int)) or (
             isinstance(ground_truth, float) and isinstance(ground_truth, float)):
         metrics['error'] = prediction - ground_truth
@@ -20,13 +32,23 @@ def get_metrics(prediction, ground_truth, metric_grids=None, debug=False):
     metrics['squared_error'] = metrics['error'] * metrics['error']
     for metric_grid in metric_grids:
         str_metric_grid = str(metric_grid[0]) + 'x' + str(metric_grid[1])
-        gape, gcae = get_grid_metrics(prediction, ground_truth, metric_grid, debug=debug)
+        gape, gcae = get_grid_metrics(prediction, ground_truth, metric_grid)
         metrics['grid' + str_metric_grid + '_absolute_percentage_error'] = gape
-        metrics['grid' + str_metric_grid + '_cell_absolute_error'] = gape
+        metrics['grid' + str_metric_grid + '_cell_absolute_error'] = gcae
     return metrics
 
 
-def get_metrics_with_points(prediction, ground_truth, metric_grids=None, debug=False):
+def get_metrics_with_points(prediction, ground_truth, metric_grids=None):
+    """
+    get metrics between prediction (np array) and ground truth (list of points)
+    metrics are absolute_error (AE), absolute_percentage_error (APE), squared_error (SE)
+    and for metrics grid (only for np array) gridNxN_absolute_percentage_error (GAPE) and  gridNxN_cell_absolute_error (GCAE) 
+    args:
+        - prediction: prediction value (np array)
+        - ground_truth: list of points [(x1,y1), (x2,y2),...]
+        - metric_grids: List of tuple [(2,2),(4,4)], default None.
+    """
+    
     metrics = dict()
     nb_total = len(ground_truth)
     metrics['error'] = prediction.sum() - nb_total
@@ -37,29 +59,26 @@ def get_metrics_with_points(prediction, ground_truth, metric_grids=None, debug=F
     metrics['squared_error'] = metrics['error'] ** 2
     for metric_grid in metric_grids:
         str_metric_grid = str(metric_grid[0]) + 'x' + str(metric_grid[1])
-        gape, gcae = get_grid_metrics_with_points(prediction, ground_truth, metric_grid, debug=debug)
+        gape, gcae = get_grid_metrics_with_points(prediction, ground_truth, metric_grid)
         metrics['grid' + str_metric_grid + '_absolute_percentage_error'] = gape
-        metrics['grid' + str_metric_grid + '_cell_absolute_error'] = gape
+        metrics['grid' + str_metric_grid + '_cell_absolute_error'] = gcae
     return metrics
 
 
-def get_grid_metrics(prediction_map, ground_truth_map, metric_grid, debug=False):
-    if debug:
-        print('metric_grid:', metric_grid)
-        print("prediction_map(sum):", prediction_map.sum())
-        print('prediction_map.shape', prediction_map.shape)
-        print('prediction_map', prediction_map)
-        print("ground_truth_map(sum):", ground_truth_map.sum())
-        print('ground_truth_map.shape:', ground_truth_map.shape)
-        print('ground_truth_map:', ground_truth_map)
+def get_grid_metrics(prediction_map, ground_truth_map, metric_grid):
+    """
+    get grid metrics between prediction (np array) and ground truth (np array)
+    metrics are gridNxN_absolute_percentage_error (GAPE) and  gridNxN_cell_absolute_error (GCAE) 
+    args:
+        - prediction_map (array) : prediction value
+        - ground_truth_map (array) : ground_truth value
+        - metric_grids: List of tuple [(2,2),(4,4)], default None.
+    """
 
     matrix_prediction_map = np.zeros(metric_grid)
 
     pm_width = prediction_map.shape[1]
     pm_height = prediction_map.shape[0]
-    if debug:
-        print('pm_width:', pm_width)
-        print('pm_height:', pm_height)
 
     n_w = int(math.ceil(pm_width / metric_grid[0]))
     n_h = int(math.ceil(pm_height / metric_grid[1]))
@@ -79,18 +98,11 @@ def get_grid_metrics(prediction_map, ground_truth_map, metric_grid, debug=False)
 
             sub_prediction_map = prediction_map[y_start:y_stop, x_start:x_stop]
             matrix_prediction_map[iw, ih] = sub_prediction_map.sum()
-            if debug:
-                print('iw:', iw, 'x_start:', x_start, 'x_stop:', x_stop, 'ih:', ih, 'y_start:', y_start, 'y_stop:',
-                      y_stop)
-                print("sub_prediction_map(sum):", sub_prediction_map.sum())
 
     matrix_ground_truth_map = np.zeros(metric_grid)
 
     gtm_width = ground_truth_map.shape[1]
     gtm_height = ground_truth_map.shape[0]
-    if debug:
-        print('gtm_width:', gtm_width)
-        print('gtm_height:', gtm_height)
 
     n_w = int(math.ceil(gtm_width / metric_grid[0]))
     n_h = int(math.ceil(gtm_height / metric_grid[1]))
@@ -110,47 +122,34 @@ def get_grid_metrics(prediction_map, ground_truth_map, metric_grid, debug=False)
 
             sub_ground_truth_map = ground_truth_map[y_start:y_stop, x_start:x_stop]
             matrix_ground_truth_map[iw, ih] = sub_ground_truth_map.sum()
-            if debug:
-                print('iw:', iw, 'x_start:', x_start, 'x_stop:', x_stop, 'ih:', ih, 'y_start:', y_start, 'y_stop:',
-                      y_stop)
-                print("sub_ground_truth_map(sum):", sub_ground_truth_map.sum())
-
-    if debug:
-        print('matrix_ground_truth_map:', matrix_ground_truth_map)
-        print('matrix_prediction_map:', matrix_prediction_map)
 
     matrix_difference = matrix_prediction_map - matrix_ground_truth_map
-
-    if debug:
-        print('matrix_difference:', matrix_difference)
 
     matrix_final = matrix_difference.round()
     matrix_final = np.absolute(matrix_final)
 
-    if debug:
-        print('matrix_final:', matrix_final)
-
     gt_nb_person = ground_truth_map.sum()
     if gt_nb_person == 0:
         gt_nb_person = 1
-    if debug:
-        print('gt_nb_person:', gt_nb_person)
 
     # grid absolute percentage error
     gape = 100. * matrix_final.sum() / gt_nb_person
-    if debug:
-        print('gape:', gape)
 
     # grid cell absolute error
     gcae = (matrix_final.sum() / metric_grid[0] / metric_grid[1]).round()
-    if debug:
-        print('gcae:', gcae)
 
     return gape, gcae
 
 
-def get_grid_metrics_with_points(prediction_map, ground_truth_points, metric_grid, debug=False):
-    # Attention si prediction_map plus petite que l'image initiale, ca va foirer
+def get_grid_metrics_with_points(prediction_map, ground_truth_points, metric_grid):
+    """
+    get grid metrics between prediction (np array) and ground truth (list of points)
+    metrics gridNxN_absolute_percentage_error (GAPE) and  gridNxN_cell_absolute_error (GCAE) 
+    args:
+        - prediction_map (array) : prediction value
+        - ground_truth_map (array) : ground_truth value
+        - metric_grids: List of tuple [(2,2),(4,4)], default None.
+    """
 
     matrix_ground_truth_points = np.zeros(metric_grid)
 
@@ -177,22 +176,13 @@ def get_grid_metrics_with_points(prediction_map, ground_truth_points, metric_gri
             for (xx, yy) in ground_truth_points:
                 if xx < x_start or xx >= x_stop or yy < y_start or yy >= y_stop:
                     continue
-                if debug:
-                    print("point:", xx, yy)
                 nb_points += 1
             matrix_ground_truth_points[iw, ih] = nb_points
-            if debug:
-                print('iw:', iw, 'x_start:', x_start, 'x_stop:', x_stop, 'ih:', ih, 'y_start:', y_start, 'y_stop:',
-                      y_stop)
-                print("nb_points:", nb_points)
 
     matrix_prediction_map = np.zeros(metric_grid)
 
     pm_width = prediction_map.shape[1]
     pm_height = prediction_map.shape[0]
-    if debug:
-        print('pm_width:', pm_width)
-        print('pm_height:', pm_height)
 
     n_w = int(math.ceil(pm_width / metric_grid[0]))
     n_h = int(math.ceil(pm_height / metric_grid[1]))
@@ -212,46 +202,56 @@ def get_grid_metrics_with_points(prediction_map, ground_truth_points, metric_gri
 
             sub_prediction_map = prediction_map[y_start:y_stop, x_start:x_stop]
             matrix_prediction_map[iw, ih] = sub_prediction_map.sum()
-            if debug:
-                print('iw:', iw, 'x_start:', x_start, 'x_stop:', x_stop, 'ih:', ih, 'y_start:', y_start, 'y_stop:',
-                      y_stop)
-                print("sub_prediction_map(sum):", sub_prediction_map.sum())
 
-    if debug:
-        print('matrix_ground_truth_points:', matrix_ground_truth_points)
-        print('matrix_prediction_map:', matrix_prediction_map)
 
     matrix_difference = matrix_prediction_map - matrix_ground_truth_points
-
-    if debug:
-        print('matrix_difference:', matrix_difference)
 
     matrix_final = matrix_difference.round()
     matrix_final = np.absolute(matrix_final)
 
-    if debug:
-        print('matrix_final:', matrix_final)
-
     gt_nb_person = len(ground_truth_points)
     if gt_nb_person == 0:
         gt_nb_person = 1
-    if debug:
-        print('gt_nb_person:', gt_nb_person)
 
     # grid absolute percentage error
     gape = 100. * matrix_final.sum() / gt_nb_person
-    if debug:
-        print('gape:', gape)
 
     # grid cell absolute error
     gcae = (matrix_final.sum() / metric_grid[0] / metric_grid[1]).round()
-    if debug:
-        print('gcae:', gcae)
 
     return gape, gcae
 
 
-def get_benchmark_metrics(data_iterator, metric_grids=None, debug=False):
+def get_benchmark_metrics(data_iterator, metric_grids=None):
+    """
+    get metrics for a bench of values given by an iterator l
+    
+    metrics are :
+    mean_absolute_error (MAE)
+    mean_absolute_percentage_error (MAPE)
+    root_mean_squared_error (RMSE) 
+    and for grid metrics :
+    mean gridNxN_absolute_percentage_error (MGAPE) 
+    gridNxN_cell_absolute_error (MGCAE) 
+    args:
+        - data_iterator (iterator) : The iterator must flow list of predictions, ground truths and precise the type of ground truth
+            
+            def data_iterator(predictions, ground_truths, ground_truth_type):
+                for i, value in enumerate(predictions):
+                    prediction = value
+                    ground_truth = ground_truths[i]
+                    yield {'ground_truth_type':ground_truth_type,
+                           'prediction':prediction,
+                           'ground_truth':ground_truth}
+        
+        prediction type     ground truth                                 ground_truth_type             metric_grids
+         np array            np arrray                                      density_maps                 List of tuple [(2,2),(4,4)], default None.
+         np array            list of points [(x1,y1), (x2,y2),...]          points                       List of tuple [(2,2),(4,4)], default None.
+         float               float                                          values                       None
+         int                 int                                            values                       None
+        - ground_truth_map (array) : ground_truth value
+        - metric_grids: List of tuple [(2,2),(4,4)], default None.
+    """
     grid_metrics_initialized = False
     metrics = {'mean_absolute_error': [], 'mean_absolute_percentage_error': [], 'root_mean_squared_error': []}
     for data in data_iterator:
@@ -273,13 +273,11 @@ def get_benchmark_metrics(data_iterator, metric_grids=None, debug=False):
         if ground_truth_type == 'points':
             internal_metrics = get_metrics_with_points(prediction,
                                                        ground_truth,
-                                                       metric_grids=metric_grids,
-                                                       debug=debug)
+                                                       metric_grids=metric_grids)
         elif ground_truth_type in ['density_maps', 'values']:  # density_maps or int or float
             internal_metrics = get_metrics(prediction,
                                            ground_truth,
-                                           metric_grids=metric_grids,
-                                           debug=debug)
+                                           metric_grids=metric_grids)
         metrics['mean_absolute_error'].append(internal_metrics['absolute_error'])
         metrics['mean_absolute_percentage_error'].append(internal_metrics['absolute_percentage_error'])
         metrics['root_mean_squared_error'].append(internal_metrics['squared_error'])
